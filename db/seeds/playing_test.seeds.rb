e686ae7e20c63c8edfdbfff16d1c306643c5c73d
zapic0 <zapico@gmail.com>
2014-07-25 23:04:50 +0200

    Seeds for start playing with data

---
+++ db/seeds/playing_test.seeds.rb
@@ -0,0 +1,10 @@
+papeles = Clue.create! name: 'El presidente del Senado reconoce un pago anotado por Bárcenas', url: "http://politica.elpais.com/politica/2013/01/31/actualidad/1359634766_210374.html"
+
+gurtel = Plot.create! name: 'Papeles de Bárcenas'
+
+barcenas = Person.create! name: 'BÁRCENAS GUTIÉRREZ, LUIS'
+pio = Person.where(name: 'GARCÍA-ESCUDERO MÁRQUEZ, PÍO').first
+
+gurtel.clues << papeles
+papeles.persons << barcenas
+papeles.persons << pio
