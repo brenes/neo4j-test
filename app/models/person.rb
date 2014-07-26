0eb07b510f5e4ad98a9d93d8d7118a58e9a8d967
David Brenes <davidjbrenes@gmail.com>
2014-07-26 16:19:27 +0200

    Definiendo una relación from en base a una to

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,6 @@
 class Person
   include Neo4j::ActiveNode
   property :name, type: String
-  has_n(:clues).from(Clue)
+  has_n(:clues).from(Clue, :persons)
   has_n(:organizations).from(Organization)
 end
