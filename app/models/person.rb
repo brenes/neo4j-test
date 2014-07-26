03cb1a8f925b427eb7d01593dd7e1297fba00667
David Brenes <davidjbrenes@gmail.com>
2014-07-26 16:19:52 +0200

    Definido un método allies con una query con path de longitud variable

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,4 +3,8 @@ class Person
   property :name, type: String
   has_n(:clues).from(Clue, :persons)
   has_n(:organizations).from(Organization)
+
+  def allies n = 1
+    query_as(:origin).match("p = origin<-[:`Clue#persons`]-(clue)-[:`Clue#persons`*1..#{n}]-(ally: Person)")
+  end
 end
